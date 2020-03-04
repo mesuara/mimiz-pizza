27d40679bdc811100eb80a44e138bf66d7da3996
mesuara <mesuara@gmail.com>
2020-03-04 12:07:28 -0500

    Updates

---
+++ precache-manifest.fb9d75d742a3bc4784219a4b24f1343d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd5e1fb2fbc18f7a631fb5b2f1c40b03",
+    "url": "/mimiz-pizza/index.html"
+  },
+  {
+    "revision": "6f4afcc8f65261934447",
+    "url": "/mimiz-pizza/static/css/main.5be96be1.chunk.css"
+  },
+  {
+    "revision": "1d6ec2f38b68702df21c",
+    "url": "/mimiz-pizza/static/js/2.4bec7612.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/mimiz-pizza/static/js/2.4bec7612.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60a9cc0ecfd975002c8f",
+    "url": "/mimiz-pizza/static/js/3.32a70ce6.chunk.js"
+  },
+  {
+    "revision": "6f4afcc8f65261934447",
+    "url": "/mimiz-pizza/static/js/main.25389bc5.chunk.js"
+  },
+  {
+    "revision": "08ceab88da18338b8b1e",
+    "url": "/mimiz-pizza/static/js/runtime-main.7c334821.js"
+  },
+  {
+    "revision": "6eb3ba00e80e2e08dce042d0c64c2387",
+    "url": "/mimiz-pizza/static/media/dough.6eb3ba00.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mimiz-pizza/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "825e41e985bedaeb520e4e4762d23e3c",
+    "url": "/mimiz-pizza/static/media/logo.825e41e9.png"
+  },
+  {
+    "revision": "42218fc6e30d48220ef5e0959ab8ef65",
+    "url": "/mimiz-pizza/static/media/pizzaAnimation.42218fc6.gif"
+  }
+]);
